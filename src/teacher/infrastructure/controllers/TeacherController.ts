b8a3c8ff6739e74b163aa95f52528a5de4dabbde
isrzian <shadow-sergej@ya.ru>
2023-06-18 19:38:30 +0700

    Новые методы контроллера Преподавателей

--- src/teacher/infrastructure/controllers/TeacherController.ts
+++ src/teacher/infrastructure/controllers/TeacherController.ts
@@ -1,6 +1,9 @@
-import {Controller, Get, Query} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Query} from '@nestjs/common';
+import {ApiBody, ApiOkResponse, ApiQuery} from '@nestjs/swagger';
 import {TeacherService} from '../../domain/services/TeacherService';
 import {TeacherSearchDto} from '../../domain/dtos/TeacherSearchDto';
+import {TeacherModel} from '../../domain/models/TeacherModel';
+import {TeacherSaveDto} from '../../domain/dtos/TeacherSaveDto';
 
 @Controller('/teacher')
 export class TeacherController {
@@ -10,9 +13,41 @@ export class TeacherController {
     }
 
     @Get()
+    @ApiQuery({type: TeacherSearchDto})
+    @ApiOkResponse({type: TeacherModel, isArray: true})
     async search(
         @Query() dto: TeacherSearchDto,
     ) {
         return this.service.search(dto);
+    }
+
+    @Get('/:id')
+    @ApiOkResponse({type: TeacherModel, isArray: false})
+    async getOne(@Param('id') id: number) {
+        return this.service.findById(id);
+    }
+
+    @Post()
+    @ApiBody({type: TeacherSaveDto})
+    @ApiOkResponse({type: TeacherModel, isArray: false})
+    async create(
+        @Body() dto: TeacherSaveDto,
+    ) {
+        return this.service.create(dto);
+    }
+
+    @Post('/:id')
+    @ApiBody({type: TeacherSaveDto})
+    @ApiOkResponse({type: TeacherModel, isArray: false})
+    async update(
+        @Body() dto: TeacherSaveDto,
+        @Param('id') id: number,
+    ) {
+        return this.service.update(id, dto);
+    }
+
+    @Delete('/:id')
+    async delete(@Param('id') id: number) {
+        return this.service.remove(id);
     }
 }
