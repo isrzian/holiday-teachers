29c7d6b63fad03d718635721149095a9da3dd765
Dmtitry Krasnov <dkrasnovdev@gmail.com>
2023-06-26 19:11:02 +0700

    edit button colorScheme

--- client/src/components/Group.tsx
+++ client/src/components/Group.tsx
@@ -118,6 +118,7 @@ export default function Group({
                   aria-label={`Редактировать ${data.name}`}
                   variant="outline"
                   onClick={onOpen}
+                  colorScheme="green"
                 >
                   <FiEdit />
                 </IconButton>
